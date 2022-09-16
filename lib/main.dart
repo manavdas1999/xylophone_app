08f98edef25a485d61d2d57564507be299f9d0f0
manav <dascorp@gmail.com>
2022-09-16 18:29:01 +0530

    base app completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: SafeArea(
+        child: Column(
+          children: [
+            buildColorTile(Colors.red, 1),
+            buildColorTile(Colors.orange, 2),
+            buildColorTile(Colors.yellow, 3),
+            buildColorTile(Colors.green, 4),
+            buildColorTile(Colors.blue, 5),
+            buildColorTile(Colors.indigo, 6),
+            buildColorTile(Colors.purple, 7),
+          ],
+        ),
+      ),
     );
   }
-}
 
+  Expanded buildColorTile(Color tileColor, int audioNum) {
+    return Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(0),
+                ),
+                onPressed: () {
+                  playAudio(audioNum);
+                },
+                child: Container(
+                  color: tileColor,
+                ),
+              ),
+            );
+  }
+
+  void playAudio(int num){
+    final player = AudioPlayer();
+    player.play(AssetSource("note$num.wav"));
+  }
+
+}
